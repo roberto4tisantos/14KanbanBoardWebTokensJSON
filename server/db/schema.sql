c5f7d75d54248fe474ee5e4a3a75d7685f04567f
FORTIS Roberto <roberto.4ti@gmail.com>
2024-12-28 12:39:53 -0500

    update

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -3,3 +3,15 @@ DROP DATABASE IF EXISTS kanban_db;
 
 -- CREATE DATABASE
 CREATE DATABASE kanban_db;
+
+-- Switch to the "kanban_db" database
+\c kanban_db;
+
+-- Create the "users" table
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
